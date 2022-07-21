4aafc11101355c6980a9dcd241865cbbf1d156dc
Gianmarco G <giangarof98@gmail.com>
2022-07-20 22:30:16 -0400

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,167 @@
+<template>
+
+    <footer>
+        <div class="container">
+            <div class="left">
+                <div class="col-1">
+                    <router-link class="header" to="#">FireBlogs</router-link>
+                        <ul>
+                            <li>
+                            <a href="#"><youTube class="svg-icon"/></a>
+                            </li>
+                            <li>
+                            <a href="#"><twitter class="svg-icon"/></a>
+                            </li>
+                            <li>
+                            <a href="#"><instagram class="svg-icon"/></a>
+                            </li>
+                            <li>
+                            <a href="#"><linkedin class="svg-icon"/></a>
+                            </li>
+                        </ul>
+                </div>
+                <div class="col-2">
+                    <ul>
+                        <router-link class="link" to="#">Home</router-link>
+                        <router-link class="link" to="#">Blogs</router-link>
+                        <router-link v-if="admin" class="link" to="#">Create Post</router-link>
+                        <router-link v-if="!user" class="link" to="#">Login In / Register</router-link>
+                    </ul>
+                </div>
+            </div>
+            <div class="right">
+                <p>Copyright 2021 All Rights Reserved</p>
+            </div>
+        </div>
+    </footer>
+    
+</template>
+
+<style scoped>
+
+footer{
+    margin-top: auto;
+    padding: 100px 25px;
+    background-color: #303030;
+}
+
+.container{
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+}
+
+.container > div{
+    display: flex;
+    flex: 1;
+}
+
+.left{
+    gap: 32px;
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.header{
+    text-align: center;
+    font-size: 24px;
+    color: #fff;
+    margin-bottom: 16px;
+    text-decoration: none;
+    font-weight: 600;
+}
+
+ul{
+    gap: 16px;
+    list-style: none;
+    display: flex;
+}
+
+.col-1, .col-2{
+    gap: 32px;
+    display: flex;
+    flex: 1;
+}
+
+.col-1{
+    flex-direction: column;
+}
+
+.col-1 h2{
+    text-align: center;
+}
+
+.col-1 ul{
+    margin-top: auto;
+}
+
+.col-1 ul li{
+    display: flex;
+    align-items: center;
+}
+
+.col-2 ul{
+    height: 100%;
+    justify-content: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+
+}
+
+.col-2 ul .link{
+    font-size: 16px;
+    font-weight: 500;
+    color: #fff;
+    text-decoration: none;
+}
+
+.right{
+    gap: 32px;
+    color: #fff;
+    align-items: center;
+    flex-direction: column;
+}
+
+p{
+    margin-top: auto;
+}
+
+@media (min-width:800px){
+    .container{
+        flex-direction: row;
+        gap: 0px;
+    }
+
+    .left{
+        flex-direction: row;
+        align-items: initial;
+        gap: 0;
+    }
+
+    .header{
+        text-align: initial;
+    }
+
+    .col-1, .col-2{
+        gap: 0;
+    }
+
+    h2{
+        text-align: initial;
+    }
+
+    .col-2 ul{
+        flex-direction: column;
+    }
+
+    .right{
+        align-items: flex-end;
+        gap: 0;
+    }
+}
+
+
+
+</style>
