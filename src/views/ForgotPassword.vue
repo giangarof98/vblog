d1ba0e69463794e99cb38f139b636a46cc3bdfc6
Gianmarco G <giangarof98@gmail.com>
2022-07-27 22:27:04 -0400

    forgotpassword style

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,15 +1,43 @@
 <template>
-    <div>
+    <div class="reset-password">
+        <div class="form-wrap">
+            <form class="reset">
+                <h2>Reset Password</h2>
+                <p>Forgot password? Please enter email to reset it</p>
+                <div class="inputs">
+                    <div class="input">
+                        <input type="text" placeholder="Email" v-model="email">
+                    </div>
+                </div>
+                <button>Reset</button>
+                <div class="angle"></div>
+            </form>
+            <div class="background"></div>
+        </div>
 
     </div>
 </template>
 
 <script>
 export default {
-    name:'ForgotPassword'
+    name:'ForgotPassword',
+
 }
 </script>
 
 <style scoped>
+
+.reset-password{
+    position: relative;
+}
+
+h2{
+    margin-bottom: 8px;
+}
+
+p{
+    text-align: center;
+    margin-bottom: 32px;
+}
 
 </style>
